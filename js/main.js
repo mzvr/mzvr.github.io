da82e7fa39c028c1bd3b9894866d97fdf4fd9f90
mzvr <61644167+mzvr@users.noreply.github.com>
2021-03-08 22:05:38 +1030

    Added Octo

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/FBXLoader.js';
+
 
 function main() {
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
-    const fov = 130;
+
+    const fov = 70;
     const aspect = 2;  // the canvas default
     const near = 0.1;
-    const far = 100;
+    const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 3;
 
@@ -16,13 +19,25 @@ function main() {
     controls.target.set(0, 0, 0);
     controls.update();
 
+    const mainLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+
+
+    //Set up shadow properties for the light
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    mainLight.castShadow = true;
+    mainLight.shadow.mapSize.width = 2048; // default
+    mainLight.shadow.mapSize.height = 2048; // default
+    mainLight.shadow.camera.near = 0.5; // default
+    mainLight.shadow.camera.far = 500; // default
+
     const scene = new THREE.Scene();
     {
         const color = 0xFFFFFF;
         const intensity = 1;
         const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(-1, 2, 4);
-        scene.add(light);
+        mainLight.position.set(-1, 2, 4);
+        scene.add(mainLight);
     }
 
     const boxWidth = 1;
@@ -41,11 +56,29 @@ function main() {
         return cube;
     }
 
+    const planeGeometry = new THREE.PlaneGeometry( 5, 5, );
+    const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+    planeGeometry.rotateX(-3.14/2);
+    const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    plane.receiveShadow = true;
+    scene.add( plane );
+
     const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
+        //makeInstance(geometry, 0x44aa88,  0),
+        //makeInstance(geometry, 0x8844aa, -2),
+        //makeInstance(geometry, 0xaa8844,  2),
     ];
+
+    {
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load(
+            'assets/textures/skybox.png',
+            () => {
+                const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+                rt.fromEquirectangularTexture(renderer, texture);
+                scene.background = rt;
+            });
+    }
 
     {
         const loader = new THREE.TextureLoader();
@@ -69,6 +102,55 @@ function main() {
         return needResize;
     }
 
+    {
+        const albedo = new THREE.TextureLoader().load( 'assets/textures/octo/octotexture.png');
+        const normal = new THREE.TextureLoader().load( 'assets/textures/octo/octonorm.png');
+        const smoothness = new THREE.TextureLoader().load( 'assets/textures/octo/octorough.png');
+
+        var lambert = new THREE.MeshStandardMaterial({color:0xffffff, map: albedo, normalMap: normal, roughnessMap: smoothness});
+
+        // model
+        const loader = new FBXLoader();
+        loader.load('assets/models/octo2.fbx', function (object) {
+
+            var geometry = [];
+
+            object.traverse(function (child) {
+                if (child.isMesh) {
+                    geometry.push(child);
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                    child.geometry.scale(0.03, 0.03, 0.03);
+                }
+            });
+
+            geometry[0].material = lambert;
+            geometry[0].translateX(1);
+            scene.add(geometry[0]);
+        });
+    }
+
+    {
+        const loader = new FBXLoader();
+        loader.load('assets/models/octo2.fbx', function (object) {
+
+            var geometry = [];
+
+            object.traverse(function (child) {
+                if (child.isMesh) {
+                    geometry.push(child);
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                    child.geometry.scale(0.03, 0.03, 0.03);
+                }
+            });
+
+            geometry[0].material = new THREE.MeshStandardMaterial({color:0xffffff});
+            geometry[0].translateX(-1);
+            scene.add(geometry[0]);
+        });
+    }
+
     function render(time) {
         time *= 0.001;
 
@@ -84,6 +166,9 @@ function main() {
             cube.rotation.x = rot;
             cube.rotation.y = rot;
         });
+
+        mainLight.position.z = Math.sin(time);
+        mainLight.position.x = Math.cos(time);
 
         renderer.render(scene, camera);
 
