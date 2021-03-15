f6c0a38ab383d5d36bec3952870a7a02b32176bb
mzvr <61644167+mzvr@users.noreply.github.com>
2021-03-16 00:12:05 +1030

    added post processing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import { FBXLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/FBXLoader.js';
 import Stats from 'https://unpkg.com/three@0.126.1/examples/jsm/libs/stats.module.js';
+import { ShaderPass } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/ShaderPass.js';
+import { EffectComposer } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'https://unpkg.com/three@0.126.1/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { GammaCorrectionShader } from 'https://unpkg.com/three@0.126.1/examples/jsm/shaders/GammaCorrectionShader.js';
+
 
 function createStats() {
     var stats = new Stats();
@@ -20,7 +26,7 @@ function main() {
 
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
-
+    const composer = new EffectComposer( renderer );
 
     const fov = 70;
     const aspect = 2;  // the canvas default
@@ -33,8 +39,8 @@ function main() {
     controls.target.set(0, 0, 0);
     controls.update();
 
-    const mainLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-
+    const mainLight = new THREE.DirectionalLight(0xFFFFFF, 3);
+    mainLight.position.set(Math.cos(0), 2, Math.sin(0));
 
     //Set up shadow properties for the light
     renderer.shadowMap.enabled = true;
@@ -45,67 +51,56 @@ function main() {
     mainLight.shadow.camera.near = 0.5; // default
     mainLight.shadow.camera.far = 200; // default
 
-
     const scene = new THREE.Scene();
+
+    scene.add(mainLight);
+
+    const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+    const renderScene = new RenderPass( scene, camera );
+
+    bloomPass.threshold = 0.6;
+    bloomPass.strength = 3;
+    bloomPass.radius = 1;
+
+    composer.addPass( renderScene );
+    composer.addPass( bloomPass );
+    //composer.addPass( new ShaderPass( GammaCorrectionShader ) );
+
+    scene.background = new THREE.Color('red');
+    //renderer.toneMapping = THREE.NoToneMapping;
     {
-        const color = 0xFFFFFF;
-        const intensity = 1;
-        const light = new THREE.DirectionalLight(color, intensity);
-        mainLight.position.set(Math.cos(0), 2, Math.sin(0));
-        scene.add(mainLight);
+        const planeGeometry = new THREE.PlaneBufferGeometry(5, 5, 1);
+        const planeMaterial = new THREE.MeshStandardMaterial({color: 0x555555})
+        planeGeometry.rotateX(-3.14 / 2);
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+        scene.add(plane);
     }
-
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    function makeInstance(geometry, color, x) {
-        const material = new THREE.MeshPhongMaterial({color});
-
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        cube.position.x = x;
-
-        return cube;
-    }
-
-    const planeGeometry = new THREE.PlaneGeometry( 5, 5, );
-    const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } )
-    planeGeometry.rotateX(-3.14/2);
-    const plane = new THREE.Mesh( planeGeometry, planeMaterial );
-    plane.receiveShadow = true;
-    scene.add( plane );
-
-    const cubes = [
-        //makeInstance(geometry, 0x44aa88,  0),
-        //makeInstance(geometry, 0x8844aa, -2),
-        //makeInstance(geometry, 0xaa8844,  2),
-    ];
 
     {
         const loader = new THREE.TextureLoader();
         const texture = loader.load(
             'https://upload.wikimedia.org/wikipedia/commons/6/60/ESO_-_Milky_Way.jpg',
             () => {
+                //texture.encoding = THREE.sRGBEncoding;
                 const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
                 rt.fromEquirectangularTexture(renderer, texture);
                 scene.background = rt;
             });
     }
 
-    function resizeRendererToDisplaySize(renderer) {
+    function resizeRendererToDisplaySize(renderer)
+    {
         const canvas = renderer.domElement;
         const width = canvas.clientWidth;
         const height = canvas.clientHeight;
         const needResize = canvas.width !== width || canvas.height !== height;
         if (needResize) {
             renderer.setSize(width, height, false);
+            composer.setSize(width, height);
         }
         return needResize;
     }
-
 
     var sun;
     {
@@ -117,9 +112,11 @@ function main() {
     }
 
     {
-        const albedo = new THREE.TextureLoader().load( 'assets/textures/octo/octotexture.png');
+        var albedo = new THREE.TextureLoader().load( 'assets/textures/octo/octotexture.png');
+        //albedo.encoding = THREE.sRGBEncoding;
         const normal = new THREE.TextureLoader().load( 'assets/textures/octo/octonorm.png');
         const smoothness = new THREE.TextureLoader().load( 'assets/textures/octo/octorough.png');
+
 
         var lambert = new THREE.MeshStandardMaterial({color:0xffffff, map: albedo, normalMap: normal, roughnessMap: smoothness});
 
@@ -144,6 +141,9 @@ function main() {
         });
     }
 
+    var octoColor = new THREE.Color("rgb(255,0,0)");
+    var octoMat = new THREE.MeshStandardMaterial({color:octoColor, roughness: 0.2});
+
     {
         const loader = new FBXLoader();
         loader.load('assets/models/octo2.fbx', function (object) {
@@ -159,11 +159,15 @@ function main() {
                 }
             });
 
-            geometry[0].material = new THREE.MeshStandardMaterial({color:0xffffff});
+            geometry[0].material = octoMat;
             geometry[0].translateX(-1);
             scene.add(geometry[0]);
         });
     }
+
+    renderer.physicallyCorrectLights = true;
+    renderer.gammaFactor = 2.2;
+    //renderer.outputEncoding = THREE.sRGBEncoding;
 
     function render(time) {
         stats.update();
@@ -176,20 +180,17 @@ function main() {
             camera.updateProjectionMatrix();
         }
 
-        cubes.forEach((cube, ndx) => {
-            const speed = 1 + ndx * .1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
-        });
-
         mainLight.position.z = 2*Math.sin(time);
         mainLight.position.x = 2*Math.cos(time);
 
         sun.position.z = 2*Math.sin(time);
         sun.position.x = 2*Math.cos(time);
 
-        renderer.render(scene, camera);
+        octoMat.color.set( new THREE.Color(Math.cos(time*5) * 0.5 + 0.5,0,0) );
+        octoMat.color.convertSRGBToLinear();
+
+        //renderer.render(scene, camera);
+        composer.render();
 
         requestAnimationFrame(render);
     }
